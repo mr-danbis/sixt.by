0057555e1e87c428319ea257295b1eaf053ef1ef
lis0jk <lissjkk@gmail.com>
2022-12-08 13:37:10 +0300

    Create block partner and fix some bugs

--- app/js/main.js
+++ app/js/main.js
@@ -1,36 +1,57 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    class Slider {
-        constructor(owlElement, owlOptions) {
-            this.owlElement = owlElement;
-            this.owlOptions = owlOptions;
-        }
-
-        addSlider(owlElement, owlOptions) {
-            $(owlElement).owlCarousel(owlOptions);
-        }
+document.addEventListener("DOMContentLoaded", function () {
+  class Slider {
+    constructor(owlElement, owlOptions) {
+      this.owlElement = owlElement;
+      this.owlOptions = owlOptions;
     }
-    const fleetSlider = new Slider();
 
-    fleetSlider.addSlider(".fleet__slider", {
-        responsive: {
-            0: {
-                items: 1,
-                margin: 10,
-            },
+    addSlider(owlElement, owlOptions) {
+      $(owlElement).owlCarousel(owlOptions);
+    }
+  }
+  const fleetSlider = new Slider();
+  const partnerSlider = new Slider();
 
-            750: {
-                items: 2,
-                margin: 20,
-            },
-            
-            1300: {
-                items: 3,
-                margin: 40,
-            },
-        }
-    });
+  fleetSlider.addSlider(".fleet__slider", {
+    responsive: {
+      0: {
+        items: 1,
+        margin: 10,
+      },
 
+      750: {
+        items: 2,
+        margin: 20,
+      },
 
+      1300: {
+        items: 3,
+        margin: 40,
+      },
+    },
+  });
 
+  partnerSlider.addSlider(".partner__slider", {
+    responsive: {
+      0: {
+        items: 2,
+        margin: 20,
+      },
+
+      750: {
+        items: 3,
+        margin: 20,
+      },
+
+      1150: {
+        items: 5,
+        margin: 20,
+      },
+
+      1640: {
+        items: 8,
+        margin: 20,
+      },
+    },
+  });
 });
