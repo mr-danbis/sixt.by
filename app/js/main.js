8b7ec4510fb3ba89a3fdbb1e615c8725d200c9a7
Daniel <dan.bisyukov@gmail.com>
2022-12-09 01:32:04 +0300

    Created reviews block

--- app/js/main.js
+++ app/js/main.js
@@ -1,57 +1,87 @@
 document.addEventListener("DOMContentLoaded", function () {
-  class Slider {
-    constructor(owlElement, owlOptions) {
-      this.owlElement = owlElement;
-      this.owlOptions = owlOptions;
+    class Slider {
+        constructor(owlElement, owlOptions) {
+            this.owlElement = owlElement;
+            this.owlOptions = owlOptions;
+        }
+
+        addSlider(owlElement, owlOptions) {
+            $(owlElement).owlCarousel(owlOptions);
+        }
     }
+    const fleetSlider = new Slider();
+    const partnerSlider = new Slider();
+    const reviewsSlider = new Slider();
 
-    addSlider(owlElement, owlOptions) {
-      $(owlElement).owlCarousel(owlOptions);
-    }
-  }
-  const fleetSlider = new Slider();
-  const partnerSlider = new Slider();
+    fleetSlider.addSlider(".fleet__slider", {
+        responsive: {
+            0: {
+                items: 1,
+                margin: 10,
+            },
 
-  fleetSlider.addSlider(".fleet__slider", {
-    responsive: {
-      0: {
-        items: 1,
-        margin: 10,
-      },
+            750: {
+                items: 2,
+                margin: 20,
+            },
 
-      750: {
-        items: 2,
-        margin: 20,
-      },
+            1300: {
+                items: 3,
+                margin: 40,
+            },
+        },
+    });
 
-      1300: {
-        items: 3,
+    partnerSlider.addSlider(".partner__slider", {
+        responsive: {
+            0: {
+                items: 2,
+                margin: 20,
+            },
+
+            750: {
+                items: 3,
+                margin: 20,
+            },
+
+            1150: {
+                items: 5,
+                margin: 20,
+            },
+
+            1640: {
+                items: 8,
+                margin: 20,
+            },
+        },
+    });
+
+    reviewsSlider.addSlider(".reviews__slider", {
         margin: 40,
-      },
-    },
-  });
+        responsive: {
+            0: {
+                items: 1,
+            },
 
-  partnerSlider.addSlider(".partner__slider", {
-    responsive: {
-      0: {
-        items: 2,
-        margin: 20,
-      },
+            850: {
+                items: 2,
+            },
 
-      750: {
-        items: 3,
-        margin: 20,
-      },
+            1400: {
+                items: 3,
+            },
+        },
+    });
 
-      1150: {
-        items: 5,
-        margin: 20,
-      },
+    function showMoreReview(){
+        const moreReviewBtn = document.querySelectorAll('.reviews__slide-more');
+        moreReviewBtn.forEach(btn => {
+            btn.addEventListener('click', ()=> {
+                
+            });
+        });
 
-      1640: {
-        items: 8,
-        margin: 20,
-      },
-    },
-  });
+    }
+    showMoreReview();
+
 });
